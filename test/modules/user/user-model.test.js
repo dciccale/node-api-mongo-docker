a6d1680cf94456ad03a8caebf91f53648054ade6
Denis Ciccale <dciccale@gmail.com>
2015-07-08 12:21:14 +0200

    chore(user-model): increase coverage

--- test/modules/user/user-model.test.js
+++ test/modules/user/user-model.test.js
@@ -4,15 +4,19 @@ var expect = require('chai').expect;
 require('../../../');
 var User = require('../../../lib/modules/user/user-model');
 
-var user = new User({
+var fakeUser = {
   provider: 'local',
   name: 'Fake User',
   email: 'test@test.com',
   password: 'password'
-});
+};
+
+var user;
 
 describe('UserModel', function () {
   beforeEach(function (done) {
+    user = new User(fakeUser);
+
     User.remove().exec().then(function () {
       done();
     });
@@ -34,7 +38,7 @@ describe('UserModel', function () {
 
   it('should fail when saving a duplicate user', function (done) {
     user.save(function () {
-      var userDup = new User(user);
+      var userDup = new User(fakeUser);
       userDup.save(function (err) {
         expect(err).to.exist;
         done();
@@ -50,11 +54,42 @@ describe('UserModel', function () {
     });
   });
 
+  it('should fail if email is already taken', function (done) {
+    user.save(function (err, user1) {
+      var userSameEmail = new User(fakeUser);
+      expect(user1.email).to.equal(userSameEmail.email);
+      userSameEmail.save(function (err, user) {
+        expect(err).to.exist;
+        expect(err.errors.email.message).to.equal('The specified email address is already in use.');
+        done();
+      });
+    });
+  });
+
+  it('should not fail if trying to save the same user', function (done) {
+    user.save(function (err, user1) {
+      var user2 = new User(user1);
+      user2.save(function (err, user) {
+        done();
+      });
+    });
+  });
+
   it('should authenticate user if password is valid', function () {
-    return expect(user.authenticate('password')).to.equal(true);
+    expect(user.authenticate(fakeUser.password)).to.equal(true);
   });
 
   it('should not authenticate user if password is invalid', function () {
-    return expect(user.authenticate('blah')).not.equal(true);
+    expect(user.authenticate('blah')).not.equal(true);
+  });
+
+  it('should get virtual password', function () {
+    expect(user.password).to.equal(fakeUser.password);
+  });
+
+  it('should get virtual token info', function () {
+    var tokenInfo = user.token;
+    expect(tokenInfo._id).to.equal(user._id);
+    expect(tokenInfo.role).to.equal(user.role);
   });
 });
